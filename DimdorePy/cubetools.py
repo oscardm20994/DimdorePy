928568fb84f32dec66dbb96b3e48c516c09ff795
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-09-01 20:55:14 +0100

    Docs update

--- DimdorePy/cubetools.py
+++ DimdorePy/cubetools.py
@@ -5,85 +5,84 @@ Created on Jan 11 17:13:01 2020
 
 @author: Oscar Dimdore-Miles
 
-script that contains taylor made tools for processing Iris data cubes
+A set of taylor made tools for processing Iris data cubes
 
 iris reference: https://scitools-iris.readthedocs.io/en/stable/
 """
 
+#import necessary packages
 import iris
 import numpy as np
 import iris.coord_categorisation as coord_cat
+import math
 
 
-def add_times(self, cube, time):
-		"""Adds time coordinate metadata to an input iris cube. 
+def add_times(cube, tname):
+	"""Function to add time coordinate metadata to an input iris cube. 
 
-		Args
-		----------
-		cube : iris.cube.Cube object
-	    An iris cube with a time dimension.
+    Parameters
+    ----------
+	cube : iris.cube.Cube
+		iris cube with a time dimension.
+	tname : str
+		String indicating the standard name of the time dimension of the cube. E.G 'time' or 't'.
+	
+    Returns
+    -------
+	iris.cube.Cube
+		iris cube with added time coordinate metadata.
 
-		time : str
-		String indicating the standard name of the time dimension of the
-		input cube. E.G 'time' or 't' etc
+	"""
 
-		Returns
-		----------
-		cube: iris.cube.Cube object
-		same iris cube as was input with added time coordinate metadata
-
-		"""
-
-		# add dimensions for month, clim_season (indicating season e.g. 'DJF'),
-		# year, day_number (day in the year) and season_year.
-		coord_cat.add_month(cube, time, name='month')
-		coord_cat.add_season(cube, time, name='clim_season')
-		coord_cat.add_year(cube, time, name='year')
-		coord_cat.add_day_of_year(cube, time, name='day_number')
-		coord_cat.add_season_year(cube, time, name='season_year')
+	# add dimensions for month, clim_season (indicating season e.g. 'DJF'),
+	# year, day_number (day in the year) and season_year.
+	coord_cat.add_month(cube, tname, name='month')
+	coord_cat.add_season(cube, tname, name='clim_season')
+	coord_cat.add_year(cube, tname, name='year')
+	coord_cat.add_day_of_year(cube, tname, name='day_number')
+	coord_cat.add_season_year(cube, tname, name='season_year')
 		
-		return cube
+	return cube
 
 
 
-def seasonally_detrend(self, cube, tname, year_len = 360):
-		"""Function that seasonally detrends the data in an input iris cube. 
-		This is done by estimating the seasonal component to data in the cube
-		(the saesonal cycle) and subtracting this component from the data.
+def seasonally_detrend(cube, tname, year_len = 360):
+	"""Function that seasonally detrends the data in an input iris cube. 
+	This is done by estimating the seasonal component to data in the cube
+	(the saesonal cycle) and subtracting this component from the data.
 
-		Args
-		----------
-		cube : iris.cube.Cube object
+    Parameters
+    ----------
+		cube : iris.cube.Cube
     		An iris cube with a time dimension.
     
 		year_len : int
     		integer indicating the number of timepoints in each year of the
-    		cube's data. Default value is 360 day calendar in pi-control model data.
-    		Also works with 366 for daily observation data.
+    		cube's data. Default value is 360 day calendar in pi-control model 
+            data. this function Also works with 366 for daily observation data.
 
 		tname : str
     		String indicating the standard name of the time dimension of the
     		input cube. E.G 'time' or 't' etc.
 
-		Returns
-		----------
-		cube: iris.cube.Cube object
-    		same iris cube as was input with added time coordinate metadata
-
-		"""
+    Returns
+    -------
+    iris.cube.Cube
+        deseasonalised iris cube  
+	"""
         
-		# get the number of time points in the cube   
-		ntime = len(cube.coord(tname).points)
+	# get the number of time points in the cube   
+	ntime = len(cube.coord(tname).points)
 
-		# find seasonal cycle by aggregating
-		cycle = cube.aggregated_by('day_number', iris.analysis.MEAN).data,
+	# find seasonal cycle by aggregating
+	cycle = cube.aggregated_by('day_number', iris.analysis.MEAN).data,
 
-		# repeat this seasonal cycle over the time dimension for the purpose
-		# of subtracting
-		cycle_repeated = np.tile(cycle, [int(math.ceil(ntime/year_len)), 1])[0,:]
+	# repeat this seasonal cycle over the time dimension for the purpose
+	# of subtracting
+	cycle_repeated = np.tile(cycle, [int(math.ceil(ntime/year_len)), 1])[0,:]
 
-		# subtract off repeated saeasonal cycle from the cube's data
-		detrended = iris.analysis.maths.subtract(cube, cycle_repeated)
+	# subtract off repeated saeasonal cycle from the cube's data
+	detrended = iris.analysis.maths.subtract(cube, cycle_repeated)
 
-		# return the detrended cube
-		return detrended
+	# return the detrended cube
+	return detrended
--- DimdorePy/test/DimdorePy.test.py
+++ DimdorePy/test/DimdorePy.test.py
@@ -1,1 +1,21 @@
-#example text
+# testing script for DimdorePy 
+# conducts unit testing with pytest on the package
+
+import pytest
+from DimdorePy.cubetools import add_times, seasonally_detrend
+import iris
+
+#load a sample test iris cube to conduct tests on
+testing_cube = iris.load('../data/test_cube.nc')[0]
+
+def test_time_metadata_added():
+    
+    #call the add_times function
+    new_cube = add_times(testing_cube, 't')
+    
+    #assert expected metadata has been added to the cube
+    assert new_cube.coord('month') != None
+    assert new_cube.coord('clim_season') != None
+    assert new_cube.coord('year') != None
+    assert new_cube.coord('day_number') != None
+    assert new_cube.coord('season_year') != None
